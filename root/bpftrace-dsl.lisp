68101795531acaf2820551fc97df9baf64733cdb
cppnb2023 <fengxuebing2021@163.com>
2026-04-17 13:41:24 +0800

    增加更多的do变体和bpftrace的S表达式dsl

--- root/bpftrace-dsl.lisp
+++ root/bpftrace-dsl.lisp
@@ -2,13 +2,16 @@
   (:use :cl :generic)
   (:export :bpftrace-printf :bpftrace-progn :bpftrace-probe 
            :bpftrace-code :with-write-bpftrace :bpftrace-not-in
-           :bpftrace-in :str))
+           :bpftrace-in))
 
 (in-package :bpftrace-dsl)
 
 (defun generate-occupy (keyword)
+  ;;小心使用:str，因为bpftrace工具的原因没什么完美解决方案
+  ;;能不用就不用，尤其是comm，username最好去proc目录去读
+  ;;已经提供了读proc目录文件的函数了
   (ecase keyword
-;;	 (:str "\\\"%s\\\"")
+	 (:str "\\\"%s\\\"")
 	 (:i32 "%d")
 	 (:u32 "%u")
 	 (:i64 "%ld")
@@ -37,11 +40,7 @@
 (defun generate-args (plist &key (start "") (end ""))
   (flet ((-> (k v)
 			  (cond
-				;;((eq k :str)
-				;; (format nil "strreplace(~a, \"~a\", \"~a\")"
-				;;		 v
-				;;		 (generate-depth-string "\"" 1)
-				;;		 (generate-depth-string "\"" 2)))
+				;;((eq k :str)))
 				 (t v))))
 	 (do-plist-stage-format (k v plist)
 		(:first (:format "~a~a" start v))
@@ -67,15 +66,6 @@
 (defun bpftrace-probe (probe &rest exprs)
   (format nil "~a{~a}" probe (apply #'bpftrace-concatenate exprs)))
 
-(defmacro bpftrace-code (&body body)
-  `(macrolet ((:printf (idx &body kvlist) `(bpftrace-printf ,idx ,@kvlist))
-              (:progn (&body body)        `(bpftrace-progn ,@body))
-              (:probe (probe &body body)  `(bpftrace-probe ,probe ,@body)))
-     (bpftrace-concatenate ,@body)))
-
-(defmacro with-write-bpftrace ((stream) &body body)
-  `(format ,stream (bpftrace-code ,@body)))
-
 (defun bpftrace-not-in (var &rest rest)
   (do-list-stage-format (arg rest)
 	 (:first (:format "(~a != ~a" var arg))
@@ -87,4 +77,73 @@
 	 (:first (:format "(~a == ~a" var arg))
 	 (:main  (:format " || ~a != ~a" var arg))
 	 (:end   (:format ")"))))
+
+(defun bpftrace-if (cond then &optional else)
+  (with-output-to-string (s)
+	 (format s "if (~a) ~a" cond then)
+	 (when else
+		(format s "else ~a" else))))
+
+(defun bpftrace-= (&rest strings)
+  (when (or (not strings) (singlep strings))
+	 (return-from bpftrace-= (format nil "true")))
+  (do-tuple-stage-format ((left right) strings)
+	 (:first (:format "(~a == ~a" left right))
+	 (:main  (:format " && ~a == ~a" left right))
+	 (:end   (:format ")"))))
+
+(defun bpftrace-/= (&rest strings)
+  (when (or (not strings) (singlep strings))
+	 (return-from bpftrace-/= (format nil "true")))
+  (do-tuple-stage-format ((left right) strings)
+	 (:first (:format "(~a != ~a" left right))
+	 (:main  (:format " && ~a != ~a" left right))
+	 (:end   (:format ")"))))
+
+(defmacro bpftrace-cond (&body sentence)
+  (labels ((:bpftrace-cond (sentence)
+				 (unless sentence (return-from :bpftrace-cond "{}"))
+				 (let ((first (first sentence))
+						 (rest  (rest sentence)))
+					`(bpftrace-if ,(first first)
+									  (bpftrace-progn
+										,@(cdr first))
+									  ,(:bpftrace-cond rest)))))
+	 (when (nthcdr 100 sentence) 
+		(error "链表太长了"))
+	 (:bpftrace-cond sentence)))
+
+(defun bpftrace-and (&rest exprs)
+  (do-list-stage-format (e exprs)
+	 (:first (:format "(~a" e))
+	 (:main  (:format " && ~a" e))
+	 (:end   (:format ")"))))
+
+(defun bpftrace-or  (&rest exprs)
+  (do-list-stage-format (e exprs)
+	 (:first (:format "(~a" e))
+	 (:main  (:format " || ~a" e))
+	 (:end   (:format ")"))))
+
+(defmacro bpftrace-code (&body body)
+  `(macrolet ((:printf (idx &body kvlist) `(bpftrace-printf ,idx ,@kvlist))
+				  (:progn (&body body)        `(bpftrace-progn ,@body))
+				  (:probe (probe &body body)  `(bpftrace-probe ,probe ,@body))
+				  (:not-in (var &body rest) `(bpftrace-not-in ,var ,@rest))
+				  (:in (var &body rest) `(bpftrace-in ,var ,@rest))
+				  (:= (&body strings) `(bpftrace-= ,@strings))
+				  (:/= (&body strings) `(bpftrace-/= ,@strings))
+				  (:if (cond then &optional else) `(bpftrace-if ,cond ,then ,else))
+				  (:cond (&body sentence) `(bpftrace-cond ,@sentence))
+				  (:and (&body exprs) `(bpftrace-and ,@exprs))
+				  (:or (&body exprs) `(bpftrace-or ,@exprs))
+				  (:bstr (string) `(format nil "\"~a\"" ,string))
+				  (:str (var-string) `(format nil "str(~a)" ,var-string))
+				  (:ustr (var-string) `(format nil "ustr(~a)" ,var-string))
+				  (:t () "true")
+				  (:f () "false"))
+	  (bpftrace-concatenate ,@body)))
+
+(defmacro with-write-bpftrace ((stream) &body body)
+  `(format ,stream (bpftrace-code ,@body)))
 
